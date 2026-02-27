4a01934faef444588f488a6b6521da8c48c6945b
Smilie Pop <arisatominato48@gmail.com>
2026-02-27 19:10:59 +0800

    Updated backgrounds and models

--- src/_get_updated_models.py
+++ src/_get_updated_models.py
@@ -2,38 +2,42 @@ import json
 import requests
 
 character_models = {
-    "01ichika":"ichika",
-    "02saki":"saki",
-    "03honami":"honami",
-    "04shiho":"shiho",
-    "05minori":"minori",
-    "06haruka":"haruka",
-    "07airi":"airi",
-    "08shizuku":"shizuku",
-    "09kohane":"kohane",
-    "10an":"an",
-    "11akito":"akito",
-    "12touya":"touya",
-    "13tsukasa":"tsukasa",
-    "14emu":"emu",
-    "15nene":"nene",
-    "16rui":"rui",
-    "17kanade":"kanade",
-    "18mafuyu":"mafuyu",
-    "19ena":"ena",
-    "20mizuki":"mizuki",
-    "21miku":"miku",
-    "22rin":"rin",
-    "23len":"len",
-    "24luka":"luka",
-    "25meiko":"meiko",
-    "26kaito":"kaito",
+    "01ichika": "ichika",
+    "02saki": "saki",
+    "03honami": "honami",
+    "04shiho": "shiho",
+    "05minori": "minori",
+    "06haruka": "haruka",
+    "07airi": "airi",
+    "08shizuku": "shizuku",
+    "09kohane": "kohane",
+    "10an": "an",
+    "11akito": "akito",
+    "12touya": "touya",
+    "13tsukasa": "tsukasa",
+    "14emu": "emu",
+    "15nene": "nene",
+    "16rui": "rui",
+    "17kanade": "kanade",
+    "18mafuyu": "mafuyu",
+    "19ena": "ena",
+    "20mizuki": "mizuki",
+    "21miku": "miku",
+    "22rin": "rin",
+    "23len": "len",
+    "24luka": "luka",
+    "25meiko": "meiko",
+    "26kaito": "kaito",
 }
 
+
 def fetch_model_list():
-    response = requests.get("https://storage.sekai.best/sekai-live2d-assets/live2d/model_list.json")
+    response = requests.get(
+        "https://storage.sekai.best/sekai-live2d-assets/live2d/model_list.json"
+    )
     response.raise_for_status()
     return response.json()
+
 
 def arrange_models_by_character(models):
     character_data = {}
@@ -41,7 +45,9 @@ def arrange_models_by_character(models):
         model_prefix = ""
         character = "others"
         if any(cm in model["modelBase"] for cm in character_models.keys()):
-            model_prefix = [cm for cm in character_models.keys() if cm in model["modelBase"]][0]
+            model_prefix = [
+                cm for cm in character_models.keys() if cm in model["modelBase"]
+            ][0]
             character = character_models[model_prefix]
 
         if character in character_data:
@@ -50,23 +56,27 @@ def arrange_models_by_character(models):
         character_data[character] = [model]
     return character_data
 
+
 def compare_models(old_models, new_models):
     updated_models = {}
     for character, models in new_models.items():
         if character in old_models:
             new_model_ids = [model["modelName"] for model in models]
             old_model_ids = [model["modelName"] for model in old_models[character]]
-            updated_model_ids = [model_id for model_id in new_model_ids if model_id not in old_model_ids]
+            updated_model_ids = [
+                model_id for model_id in new_model_ids if model_id not in old_model_ids
+            ]
             if updated_model_ids:
                 updated_models[character] = updated_model_ids
     return updated_models
+
 
 def main():
     print("Fetching models from Sekai Live2D Assets...")
     models = fetch_model_list()
     print("Models fetched successfully.")
 
-    sorted_models = sorted(models, key=lambda x: x["modelBase"]) 
+    sorted_models = sorted(models, key=lambda x: x["modelBase"])
 
     character_data: dict[str, list[dict]] = arrange_models_by_character(sorted_models)
 
@@ -77,15 +87,26 @@ def main():
     with open("character_sekai.json", "r", encoding="utf-8") as f:
         old_character_data = json.load(f)
     updated_models = compare_models(old_character_data, character_data)
+    models_removed = compare_models(character_data, old_character_data)
 
     if not updated_models:
         print("No new models found.")
-        return
-    
-    print("New models found:")
-    for character, model_ids in updated_models.items():
-        if model_ids:
-            print(f"{character}: {model_ids}")
+    else:
+        print("New models found:")
+        for character, model_ids in updated_models.items():
+            if model_ids:
+                print(f"{character}: {model_ids}")
+                
+    print("\n")
+
+    if not models_removed:
+        print("No models removed.")
+    else:
+        print("Models removed:")
+        for character, model_ids in models_removed.items():
+            if model_ids:
+                print(f"{character}: {model_ids}")
+
 
 if __name__ == "__main__":
     main()
