cd1ec7f6f22357c689bc88ab7b616ea57fecc703
Smilie Pop <arisatominato48@gmail.com>
2026-03-05 08:17:48 +0800

    Announced new update

--- src/components/UI/Announcements.tsx
+++ src/components/UI/Announcements.tsx
@@ -23,25 +23,18 @@ const Announcements: React.FC = () => {
         <div id="announcements" onClick={handleAnnouncements}>
             <h2>Notice</h2>
             <p>
-                With 35 mentioned character ships, for today's Valentine ships
-                are...
+                "Let's stay together, Mizuki... N25's been waiting for you..."
             </p>
-            <p>
-                AkiToya, MizuKana, RuiKasa, and (as a bonus) MizuMafu and
-                HonaKana!
-            </p>
-            <p>
-                (someone also commented "mafuyu x therapy" so have that as a
-                bonus as well xD)
-            </p>
-            <br></br>
-            <p>There is one update I have done too.</p>
+            <p>Few updates has been made:</p>
             <ul>
                 <li>
-                    <p>Added "Re-apply All" button for Live2D Parameters.</p>
+                    You can now make your own choices text! (I'll be waiting
+                    your "choose your own path" stories.)
                 </li>
+                <li>Updated list of models and backgrounds.</li>
+                <li>Updated the tutorial with the recent features.</li>
             </ul>
-            <p>Happy Valentine's Day, everyone! ♡</p>
+
             <p>Tap this section to close.</p>
         </div>
     );
